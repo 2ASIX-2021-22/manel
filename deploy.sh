7537381a74eeccb695b7d4b39c15930aafac0803
manelpinol <mpinol@iesebre.com>
2022-02-04 16:18:40 +0100

    Fitxer de deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-git checkout production
+git checkout production2
 git merge main --no-edit
-git push origin production
+git push origin production2
 git checkout main
 
 # Copiat de https://forge.laravel.com/servers/512103/sites/1487951#/application
-wget https://forge.laravel.com/servers/512103/sites/1487951/deploy/http?PvB78NKBrfptI7VCwTEYJg4XXDI5aceWc9P6qxP6 -O /dev/null
+wget https://forge.laravel.com/servers/506458/sites/1555290/deploy/http?token=Yp4M53rKusqMwXWroTjt3FNkntIHFY0K2t59WqBL -O /dev/null
